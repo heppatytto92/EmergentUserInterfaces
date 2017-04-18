000961231dab0beb78c30f6df30bc20ac0510d72
hiroolt <sunasuna.8@gmail.com>
2017-04-18 16:15:26 +0300

    Arduino code has been changed

--- Hiro/Arduino/EmergentUI/EmergentUI.ino
+++ Hiro/Arduino/EmergentUI/EmergentUI.ino
@@ -1,30 +1,31 @@
 // CS-E4200 Emergent UI sample code
 // Created by Hiroki Sunagawa
+// Description: The code aims for Arduino Uno with pressure sensor, accelerometer, and gyro sensor.
+// The output is something like this.
+//     Pressure sensor: touched / not touched
+//     Accelerometer(Movement): moving / not moving
+//     Gyro sensor (Position): 0 / 1 / 2 / 3 / 4 / 5 <= These are corresponding to something like dice
+// The output format is "Bool(pressure), Bool(movement), Int(position)", for example, "true, false, 5"
 
 // This library allows you to communicate with I2C / TWI devices.
 #include<Wire.h>
 
-//setup variables for accelerometer
+//setup variables for accelerometer and gyro sensor
 const int MPU_addr=0x68;  // I2C address of the MPU-6050
-int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+int16_t AcX,AcY,AcZ,GyX,GyY,GyZ;
 
 // setup variables for pressure sensor
 int fsrAnalogPin = 0; // FSR is connected to analog 0
-int fsrReading;      // the analog reading from the FSR resistor divider
 
-// setup variables for proximity sensor
-int URPWM = 3; // PWM Output 0－25000US，Every 50US represent 1cm
-int URTRIG = 5; // PWM trigger pin
-unsigned int Distance=0;
-uint8_t EnPwmCmd[4]={0x44,0x02,0xbb,0x01};    // distance measure command
+// setup variables for the output
+bool Pressure;
+bool Movement;
+int Position;
 
 
 void setup(){
 // Accelerometer setup
   I2C_Mode_Setup();
-
-// setup for proximity sensor
-  PWM_Mode_Setup();
 
   Serial.begin(9600);
 
@@ -34,55 +35,19 @@ void setup(){
 
 void loop(){
   // Pressure sensor value
-  fsrReading = analogRead(fsrAnalogPin);
-  Serial.print("Pressure: ");
+  //fsrReading = analogRead(fsrAnalogPin);
+  //Serial.print("Pressure: ");
   //Serial.println(fsrReading);
-  Serial.print(fsrReading);
-  // Prox sensor
-  // READ_ProximitySensor();
 
-  // Accelerometor
-  READ_Accelerometor();
+  Serial.print(READ_Pressure());
+  Serial.print(", ");
+  Serial.print(READ_Accelerometor());
+  Serial.print(", ");
+  Serial.println(READ_Gyro());
   delay(500);
 }
 
 
-// PWM Mode setup for proximity sensor
-
-void PWM_Mode_Setup()
-{
-  pinMode(URTRIG,OUTPUT);                     // A low pull on pin COMP/TRIG
-  digitalWrite(URTRIG,HIGH);                  // Set to HIGH
-
-  pinMode(URPWM, INPUT);                      // Sending Enable PWM mode command
-
-  for(int i=0;i<4;i++)
-  {
-      Serial.write(EnPwmCmd[i]);
-  }
-}
-
-// A function to read proximity sensor value
-
-void READ_ProximitySensor()
-{                              // a low pull on pin COMP/TRIG  triggering a sensor reading
-    digitalWrite(URTRIG, LOW);
-    digitalWrite(URTRIG, HIGH);               // reading Pin PWM will output pulses
-
-    unsigned long DistanceMeasured=pulseIn(URPWM,LOW);
-
-    if(DistanceMeasured>=10200)
-    {              // the reading is invalid.
-      Serial.println("Invalid");
-    }
-    else
-    {
-      Distance=DistanceMeasured/50;           // every 50us low level stands for 1cm
-      Serial.print("Distance: ");
-      Serial.print(Distance);
-    }
-
-}
 
 // I2C Mode setup for accelerometer
 
@@ -95,9 +60,23 @@ void I2C_Mode_Setup()
   Wire.endTransmission(true);
 }
 
-// A function to read accelerometer value
+// A function to read pressure sensor value
+bool READ_Pressure()
+{
+  int fsrReading;      // the analog reading from the FSR resistor divider
+  fsrReading = analogRead(fsrAnalogPin);
+  if (fsrReading > 50)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
 
-void READ_Accelerometor()
+// A function to read accelerometer value
+bool READ_Accelerometor()
 {
   Wire.beginTransmission(MPU_addr);
   Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
@@ -106,15 +85,39 @@ void READ_Accelerometor()
   AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
   AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
   AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
-  Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+  //Serial.print(", AcX: "); Serial.print(AcX);
+  //Serial.print(", AcY: "); Serial.print(AcY);
+  //Serial.print(", AcZ: "); Serial.print(AcZ);
+  if ((AcX > 500) || (AcY > 500) || (AcZ > 500))
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+// A function to read gyro sensor value
+int READ_Gyro()
+{
+  int pos;
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_addr,14,true);  // request a total of 14 registers
   GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
   GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
   GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
-  Serial.print(", AcX: "); Serial.print(AcX);
-  Serial.print(", AcY: "); Serial.print(AcY);
-  Serial.print(", AcZ: "); Serial.print(AcZ);
-  //Serial.print(", Tmp: "); Serial.print(Tmp/340.00+36.53);  //equation for temperature in degrees C from datasheet
-  Serial.print(", GyX: "); Serial.print(GyX);
-  Serial.print(", GyY: "); Serial.print(GyY);
-  Serial.print(", GyZ: "); Serial.println(GyZ);
+  //Serial.print(", GyX: "); Serial.print(GyX);
+  //Serial.print(", GyY: "); Serial.print(GyY);
+  //Serial.print(", GyZ: "); Serial.println(GyZ);
+
+  //
+  // Need to write some conditions to judge which is the current position of the dice
+  //
+
+  pos = 0;
+
+  return pos;
 }
--- Hiro/Matlab/BTSerialRead.m
+++ Hiro/Matlab/BTSerialRead.m
@@ -2,22 +2,19 @@ clear all; clc;
 delete(instrfindall)
 instrreset;
 b = Bluetooth(' MUSIC_DJ',1);
-fopen(b);
-figure
-h = animatedline;
-ax = gca;
-ax.YGrid = 'on';
-stop = false;
-startTime = datetime('now');
-while ~stop
-    a = str2num(fscanf(b));
-    % Get current time
-    t =  datetime('now');
-    % Add points to animation
-    addpoints(h,datenum(t),a)
-    % Update axes
-    ax.XLim = datenum([t-seconds(15) t]);
-    datetick('x','keeplimits')
-    drawnow
+
+try
+    fopen(b);
+catch err
+    fclose(instrfind);
+    error('Make sure if Bluetooth connection is established.');
 end
+
+while true
+    %% Read buffer data
+    out = fscanf(b);
+    fprintf(out);
+    pause(0.1)
+end
+
 fclose(b);
