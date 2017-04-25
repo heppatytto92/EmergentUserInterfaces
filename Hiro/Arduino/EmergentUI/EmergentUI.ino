c9a2a456a2e9cff9a97fdcbe262cfd99e12d08d4
hiroolt <sunasuna.8@gmail.com>
2017-04-25 16:09:28 +0300

    data format changed

--- Hiro/Arduino/EmergentUI/EmergentUI.ino
+++ Hiro/Arduino/EmergentUI/EmergentUI.ino
@@ -5,7 +5,7 @@
 //     Pressure sensor: touched / not touched
 //     Accelerometer(Movement): moving / not moving
 //     Gyro sensor (Position): 0 / 1 / 2 / 3 / 4 / 5 <= These are corresponding to something like dice
-// The output format is "Bool(pressure), Bool(movement), Int(position)", for example, "true, false, 5"
+// The output format is "Bool(pressure), Bool(movement), Int(position)", for example, "1, 0, 5"
 
 // This library allows you to communicate with I2C / TWI devices.
 #include<Wire.h>
@@ -13,14 +13,16 @@
 //setup variables for accelerometer and gyro sensor
 const int MPU_addr=0x68;  // I2C address of the MPU-6050
 int16_t AcX,AcY,AcZ,GyX,GyY,GyZ;
+int16_t AcX_ = 0;
+int16_t AcY_ = 0;
+int16_t AcZ_ = 0;
 
 // setup variables for pressure sensor
 int fsrAnalogPin = 0; // FSR is connected to analog 0
 
 // setup variables for the output
-bool Pressure;
-bool Movement;
-int Position;
+bool mov;
+int pos = 1;
 
 
 void setup(){
@@ -34,10 +36,6 @@ void setup(){
 // Main function
 
 void loop(){
-  // Pressure sensor value
-  //fsrReading = analogRead(fsrAnalogPin);
-  //Serial.print("Pressure: ");
-  //Serial.println(fsrReading);
 
   Serial.print(READ_Pressure());
   Serial.print(", ");
@@ -85,23 +83,25 @@ bool READ_Accelerometor()
   AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
   AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
   AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
-  //Serial.print(", AcX: "); Serial.print(AcX);
-  //Serial.print(", AcY: "); Serial.print(AcY);
-  //Serial.print(", AcZ: "); Serial.print(AcZ);
-  if ((AcX > 500) || (AcY > 500) || (AcZ > 500))
+  
+  if ((AcX > AcX_ + 2000) || (AcX < AcX_ - 2000) || (AcY > AcY_ + 2000) || (AcY < AcY_ - 2000) || (AcZ > AcZ_ + 2000) || (AcZ < AcZ_ - 2000))
   {
-    return true;
+    mov = true;
   }
   else
   {
-    return false;
+    mov =  false;
   }
+  AcX_ = AcX;
+  AcY_ = AcY;
+  AcZ_ = AcZ;
+  
+  return mov;
 }
 
 // A function to read gyro sensor value
 int READ_Gyro()
 {
-  int pos;
   Wire.beginTransmission(MPU_addr);
   Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
   Wire.endTransmission(false);
@@ -112,12 +112,29 @@ int READ_Gyro()
   //Serial.print(", GyX: "); Serial.print(GyX);
   //Serial.print(", GyY: "); Serial.print(GyY);
   //Serial.print(", GyZ: "); Serial.println(GyZ);
-
-  //
-  // Need to write some conditions to judge which is the current position of the dice
-  //
-
-  pos = 0;
-
+  if(GyX < -15000)
+  {
+    pos = 1;
+  }
+  else if (GyX > 15000)
+  {
+    pos = 2;
+  }
+  else if (GyY < -15000)
+  {
+    pos = 3;
+  }
+  else if (GyY > 15000)
+  {
+    pos = 4;
+  }
+  else if (GyZ < -15000)
+  {
+    pos = 5;
+  }
+  else if (GyZ > 15000)
+  {
+    pos = 6;
+  }
   return pos;
 }
